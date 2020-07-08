bc87fbf8152cfc3de14a36857d07f8eca21bccdb
AgusQuintanar <agusquintanar17@gmail.com>
2020-07-08 11:58:38 -0500

    Restructure of files

--- FrontEnd/laika_web/src/index.js
+++ FrontEnd/laika_web/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
+import "Tachyons";
 
 ReactDOM.render(
   <React.StrictMode>
