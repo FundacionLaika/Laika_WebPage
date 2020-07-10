b6674a6e4691a0fea5bd85d1c2eeccbb63d5e3c7
Andruixxd31 <andruix93@gmail.com>
2020-07-10 13:13:06 -0500

    Agregue componentes de react router

--- FrontEnd/laika_web/src/Components/MenuBar/MenuBar.js
+++ FrontEnd/laika_web/src/Components/MenuBar/MenuBar.js
@@ -1,29 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./MenuBar.css";
 
 const MenuBar = () => {
-  return (
-    <nav>
-      <span>
-        <span className="flex justify-center fondo pa2">
-          <button className="f2 ph3 bw0 bg-transparent white dim pointer">
-            Consulta
-          </button>
+    return (
+        <nav>
+            <span>
+                <span className="flex justify-center fondo pa2">
+                    <Link to="../Consulta/Consulta">
+                        <button className="f2 ph3  bw0 bg-transparent white dim pointer">
+                            Consulta
+                        </button>
+                    </Link>
 
-          <button className="f2 ph3  bw0 bg-transparent white dim pointer">
-            Registro de Animal
-          </button>
-          <img
-            className="der ph2 pointer"
-            width="65"
-            height="65"
-            alt="ajustes"
-            src="/Circulo.png"
-          />
-        </span>
-      </span>
-    </nav>
-  );
+                    <Link to="../RegistroGeneral/RegistroGeneral">
+                        <button className="f2 ph3  bw0 bg-transparent white dim pointer">
+                            Registro de Animal
+                        </button>
+                    </Link>
+
+                    <img
+                        className="der ph2 pointer"
+                        width="65"
+                        height="65"
+                        alt="ajustes"
+                        src="/Circulo.png"
+                    />
+                </span>
+            </span>
+        </nav>
+    );
 };
 
 export default MenuBar;
