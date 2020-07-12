5f56033df7957cbf6ee5c86a7b7528a368160f11
AgusQuintanar <agusquintanar17@gmail.com>
2020-07-12 01:41:58 -0500

    almost finish data grid

--- FrontEnd/laika_web/src/Components/Adopcion/Adopcion.js
+++ FrontEnd/laika_web/src/Components/Adopcion/Adopcion.js
@@ -2,6 +2,7 @@ import React from "react";
 import Direccion from "../SharedComponents/Direccion";
 import DatosGenerales from "./Subcomponents/DatosGenerales";
 import Foto from "../SharedComponents/Foto";
+import DataGrid from "../SharedComponents/DataGrid/DataGrid";
 
 export default class Adopcion extends React.Component {
 
@@ -16,7 +17,7 @@ export default class Adopcion extends React.Component {
         municipio: "",
         fechaAdopcion: "",
         medioAdopcion: "",
-        dataGrid: "",
+        dataGrid: [],
         foto: "/iconoPerro.png"
 	};
 
@@ -29,7 +30,21 @@ export default class Adopcion extends React.Component {
 	handleSubmit = (event) => {
 		event.preventDefault();
 		console.log(this.state);
+    };
+    
+    addRow = (row) => {
+		this.setState((state) => ({
+			dataGrid: [row, ...state.dataGrid],
+		}));
 	};
+
+	deleteRow = (id) => {
+		this.setState((state) => ({
+			dataGrid: state.dataGrid.filter(
+				(row) => row.id !== id
+			),
+		}));
+    };
 
     render() {
         return (
@@ -56,7 +71,14 @@ export default class Adopcion extends React.Component {
                   id="fotoDefault"
                   foto={this.state.foto}
                   imageHandler={this.state.imageHandler}
-				/>
+                />
+                
+                <DataGrid 
+                    rows={this.state.dataGrid}
+					addRow={this.addRow}
+                    deleteRow={this.deleteRow}
+                    handleChange={this.modifyRow}
+                />
 
                 <button type="submit">Registrar</button>
             </form>
