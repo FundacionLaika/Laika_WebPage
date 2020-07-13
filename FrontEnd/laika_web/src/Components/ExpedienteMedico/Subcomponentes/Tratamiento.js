3cbad900544310f0e76ab10829756ca5a36b14d9
Andruixxd31 <andruix93@gmail.com>
2020-07-13 11:45:24 -0500

    Cambios Chicos

--- FrontEnd/laika_web/src/Components/ExpedienteMedico/Subcomponentes/Tratamiento.js
+++ FrontEnd/laika_web/src/Components/ExpedienteMedico/Subcomponentes/Tratamiento.js
@@ -1,60 +1,49 @@
 import React from "react";
 
-import { AgGridReact } from "ag-grid-react";
-
-import "ag-grid-community/dist/styles/ag-grid.css";
-
-import "ag-grid-community/dist/styles/ag-theme-alpine.css";
-
 class Tratamiento extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			columnDefs: [
-				{
-					headerName: "Make",
-					field: "make",
-					sortable: true,
-					filter: true,
-					editable: true,
-					checkboxSelection: true,
-				},
-				{
-					headerName: "Model",
-					field: "model",
-					sortable: true,
-					filter: true,
-					editable: true,
-				},
-				{
-					headerName: "Price",
-					field: "price",
-					sortable: true,
-					filter: true,
-					editable: true,
-				},
-			],
-			rowData: [
-				{ make: "Toyota", model: "Celica", price: 35000 },
-				{ make: "Ford", model: "Mondeo", price: 32000 },
-				{ make: "Porsche", model: "Boxter", price: 72000 },
-			],
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            columnDefs: [
+                {
+                    headerName: "Make",
+                    field: "make",
+                    sortable: true,
+                    filter: true,
+                    editable: true,
+                    checkboxSelection: true,
+                },
+                {
+                    headerName: "Model",
+                    field: "model",
+                    sortable: true,
+                    filter: true,
+                    editable: true,
+                },
+                {
+                    headerName: "Price",
+                    field: "price",
+                    sortable: true,
+                    filter: true,
+                    editable: true,
+                },
+            ],
+            rowData: [
+                { make: "Toyota", model: "Celica", price: 35000 },
+                { make: "Ford", model: "Mondeo", price: 32000 },
+                { make: "Porsche", model: "Boxter", price: 72000 },
+            ],
+        };
+    }
 
-	render() {
-		return (
-			<div
-				className="ag-theme-alpine"
-				style={{ height: "200px", width: "600px" }}
-			>
-				<AgGridReact
-					columnDefs={this.state.columnDefs}
-					rowData={this.state.rowData}
-				></AgGridReact>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div
+                className="ag-theme-alpine"
+                style={{ height: "200px", width: "600px" }}
+            ></div>
+        );
+    }
 }
 
 export default Tratamiento;
