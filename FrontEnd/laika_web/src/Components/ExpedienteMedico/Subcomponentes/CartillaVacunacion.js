591958163a5a0bf7a3ace9dc89b8c4eeca6ebe71
AgusQuintanar <agusquintanar17@gmail.com>
2020-07-14 00:48:30 -0500

    added new radio buttons in expediente medico

--- FrontEnd/laika_web/src/Components/ExpedienteMedico/Subcomponentes/CartillaVacunacion.js
+++ FrontEnd/laika_web/src/Components/ExpedienteMedico/Subcomponentes/CartillaVacunacion.js
@@ -4,10 +4,9 @@ import "../Styles/Checkboxes.css";
 class CartillaVacunacion extends Component {
 	render() {
 		return (
-			<div className="cartillaVacunacion cbx">
+			<div className="cartillaVacunacion">
 				<div className="puppy">
 					<div className="cb">
-			
 						<input
 							type="checkbox"
 							id="puppy"
@@ -35,78 +34,123 @@ class CartillaVacunacion extends Component {
 						/>
 					</div>
 				</div>
-				<div className="refuerzoPuppy">
-					<input
-						type="checkbox"
-						id="refuerzoPuppy"
-						name="refuerzoPuppy"
-						value="refuerzoPuppy"
-						checked={this.props.refuerzoPuppy}
-						onChange={this.props.handleChange}
-					/>
-					<label htmlFor="refuerzoPuppy">Refuerzo Puppy</label>
 
-					<input
-						type="date"
-						id="fechaRefuerzoPuppy"
-						name="fechaRefuerzoPuppy"
-						value={this.props.fechaRefuerzoPuppy}
-						onChange={this.props.handleChange}
-					/>
+				<div className="refuerzoPuppy">
+					<div className="cb">
+						<input
+							type="checkbox"
+							id="refuerzoPuppy"
+							name="refuerzoPuppy"
+							value="refuerzoPuppy"
+							checked={this.props.refuerzoPuppy}
+							onChange={this.props.handleChange}
+						/>
+						<label htmlFor="refuerzoPuppy">
+							<span></span>
+						</label>
+					</div>
+
+					<div id="lb">
+						<label>Refuerzo Puppy</label>
+					</div>
+
+					<div id="dt">
+						<input
+							type="date"
+							id="fechaRefuerzoPuppy"
+							name="fechaRefuerzoPuppy"
+							value={this.props.fechaRefuerzoPuppy}
+							onChange={this.props.handleChange}
+						/>
+					</div>
 				</div>
+
 				<div className="multiple">
-					<input
-						type="checkbox"
-						id="multiple"
-						name="multiple"
-						value="multiple"
-						checked={this.props.multiple}
-						onChange={this.props.handleChange}
-					/>
-					<label htmlFor="multiple">Múltiple</label>
-					<input
-						type="date"
-						id="fechaMultiple"
-						name="fechaMultiple"
-						value={this.props.fechaMultiple}
-						onChange={this.props.handleChange}
-					/>
+					<div className="cb">
+						<input
+							type="checkbox"
+							id="multiple"
+							name="multiple"
+							value="multiple"
+							checked={this.props.multiple}
+							onChange={this.props.handleChange}
+						/>
+						<label htmlFor="multiple">
+							<span></span>
+						</label>
+					</div>
+
+					<div id="lb">
+						<label>Múltiple</label>
+					</div>
+
+					<div id="dt">
+						<input
+							type="date"
+							id="fechaMultiple"
+							name="fechaMultiple"
+							value={this.props.fechaMultiple}
+							onChange={this.props.handleChange}
+						/>
+					</div>
 				</div>
 				<div className="refuerzoMultiple">
-					<input
-						type="checkbox"
-						id="refuerzoMultiple"
-						name="refuerzoMultiple"
-						value="refuerzoMultiple"
-						checked={this.props.refuerzoMultiple}
-						onChange={this.props.handleChange}
-					/>
-					<label htmlFor="refuerzoMultiple">Refuerzo Múltiple</label>
-					<input
-						type="date"
-						id="fechaRefuerzoMultiple"
-						name="fechaRefuerzoMultiple"
-						value={this.props.fechaRefuerzoMultiple}
-						onChange={this.props.handleChange}
-					/>
+					<div className="cb">
+						<input
+							type="checkbox"
+							id="refuerzoMultiple"
+							name="refuerzoMultiple"
+							value="refuerzoMultiple"
+							checked={this.props.refuerzoMultiple}
+							onChange={this.props.handleChange}
+						/>
+						<label htmlFor="refuerzoMultiple">
+							<span></span>
+						</label>
+					</div>
+
+					<div id="lb">
+						<label>Refuerzo Múltiple</label>
+					</div>
+
+					<div id="dt">
+						<input
+							type="date"
+							id="fechaRefuerzoMultiple"
+							name="fechaRefuerzoMultiple"
+							value={this.props.fechaRefuerzoMultiple}
+							onChange={this.props.handleChange}
+						/>
+					</div>
 				</div>
 				<div className="rabia">
-					<input
-						type="checkbox"
-						id="rabia"
-						name="rabia"
-						value="rabia"
-						checked={this.props.rabia}
-						onChange={this.props.handleChange}
-					/>
-					<label htmlFor="rabia">Rabia</label>
-					<input
-						type="date"
-						id="fechaRabia"
-						name="fechaRabia"
-						value={this.props.fechaRabia}
-						onChange={this.props.handleChange}
-					/>
+					<div className="cb">
+						<input
+							type="checkbox"
+							id="rabia"
+							name="rabia"
+							value="rabia"
+							checked={this.props.rabia}
+							onChange={this.props.handleChange}
+						/>
+						<label htmlFor="rabia">
+							<span></span>
+						</label>
+					</div>
+
+					<div id="lb">
+						<label>Rabia</label>
+					</div>
+
+					<div id="dt">
+						<input
+							type="date"
+							id="fechaRabia"
+							name="fechaRabia"
+							value={this.props.fechaRabia}
+							onChange={this.props.handleChange}
+						/>
+					</div>
 				</div>
 			</div>
 		);
