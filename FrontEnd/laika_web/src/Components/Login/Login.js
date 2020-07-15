f3eadf37aa715b8d48dcea19e9b74c037e2c9550
Andruixxd31 <andruix93@gmail.com>
2020-07-15 12:38:35 -0500

    style changes

--- FrontEnd/laika_web/src/Components/Login/Login.js
+++ FrontEnd/laika_web/src/Components/Login/Login.js
@@ -71,7 +71,7 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div className="">
+            <div>
                 <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 bg-light-green center">
                     <main className="pa4 black-80">
                         <div className="measure">
