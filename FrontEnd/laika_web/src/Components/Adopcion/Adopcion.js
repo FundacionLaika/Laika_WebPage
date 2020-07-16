beed79fb747669780d4059f523619a77c1e9549b
ReyVega <a01114523@itesm.mx>
2020-07-15 22:53:23 -0500

    Bug de imagenes al cancelar resuelto, implementacion de datepicker en registro general

--- FrontEnd/laika_web/src/Components/Adopcion/Adopcion.js
+++ FrontEnd/laika_web/src/Components/Adopcion/Adopcion.js
@@ -9,178 +9,179 @@ import shortid from "shortid";
 import "./Styles/Adopcion.css";
 
 export default class Adopcion extends React.Component {
-    state = {
-        visitaDeAdopcion: "",
-        adoptante: "",
-        adoptado: "",
-        telefono: "",
-        calle: "",
-        numero: "",
-        colonia: "",
-        municipio: "",
-        fechaAdopcion: "",
-        medioAdopcion: "",
-        comentarios: [],
-        foto: "/iconoPerro.png",
-    };
+	state = {
+		visitaDeAdopcion: "",
+		adoptante: "",
+		adoptado: "",
+		telefono: "",
+		calle: "",
+		numero: "",
+		colonia: "",
+		municipio: "",
+		fechaAdopcion: "",
+		medioAdopcion: "",
+		comentarios: [],
+		foto: "/iconoPerro.png",
+	};
 
-    handleChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value,
-        });
-    };
+	handleChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	};
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-    };
+	handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(this.state);
+	};
 
-    addRow = (event) => {
-        event.preventDefault();
-        const newRow = {
-            id: shortid.generate(),
-            observaciones: "",
-            accion: "",
-            fecha: "",
-        };
-        this.setState((state) => ({
-            comentarios: [newRow, ...state.comentarios],
-        }));
-    };
+	addRow = (event) => {
+		event.preventDefault();
+		const newRow = {
+			id: shortid.generate(),
+			observaciones: "",
+			accion: "",
+			fecha: "",
+		};
+		this.setState((state) => ({
+			comentarios: [newRow, ...state.comentarios],
+		}));
+	};
 
-    deleteRow = (id) => {
-        this.setState((state) => ({
-            comentarios: state.comentarios.filter((row) => row.id !== id),
-        }));
-    };
+	deleteRow = (id) => {
+		this.setState((state) => ({
+			comentarios: state.comentarios.filter((row) => row.id !== id),
+		}));
+	};
 
-    modifyRow = (event) => {
-        let dataTemp = this.state.comentarios;
+	modifyRow = (event) => {
+		let dataTemp = this.state.comentarios;
 
-        dataTemp.forEach((element) => {
-            if (element.id === event.target.id) {
-                if (event.target.name === "observaciones")
-                    element.observaciones = event.target.value;
-                else if (event.target.name === "accion")
-                    element.accion = event.target.value;
-                else if (event.target.name === "fecha")
-                    element.fecha = event.target.value;
-            }
-        });
+		dataTemp.forEach((element) => {
+			if (element.id === event.target.id) {
+				if (event.target.name === "observaciones")
+					element.observaciones = event.target.value;
+				else if (event.target.name === "accion")
+					element.accion = event.target.value;
+				else if (event.target.name === "fecha")
+					element.fecha = event.target.value;
+			}
+		});
 
-        this.setState({
-            comentarios: dataTemp,
-        });
-    };
+		this.setState({
+			comentarios: dataTemp,
+		});
+	};
 
-    handleRestablecer = () => {
-        this.setState({
-            visitaDeAdopcion: "",
-            adoptante: "",
-            adoptado: "",
-            telefono: "",
-            calle: "",
-            numero: "",
-            colonia: "",
-            municipio: "",
-            fechaAdopcion: "",
-            medioAdopcion: "",
-            comentarios: [],
-            foto: "/iconoPerro.png",
-        });
-    };
+	handleRestablecer = () => {
+		this.setState({
+			visitaDeAdopcion: "",
+			adoptante: "",
+			adoptado: "",
+			telefono: "",
+			calle: "",
+			numero: "",
+			colonia: "",
+			municipio: "",
+			fechaAdopcion: "",
+			medioAdopcion: "",
+			comentarios: [],
+			foto: "/iconoPerro.png",
+		});
+	};
 
-    imageHandler = (event) => {
-        const reader = new FileReader();
-        const foto = event.target.id;
+	imageHandler = (event) => {
+		try {
+			const reader = new FileReader();
+			const foto = event.target.id;
 
-        reader.onload = () => {
-            if (reader.readyState === 2) {
-                this.setState({ [foto]: reader.result });
-            }
-        };
-        console.log(event.target.id);
-        reader.readAsDataURL(event.target.files[0]);
-    };
+			reader.onload = () => {
+				if (reader.readyState === 2) {
+					this.setState({ [foto]: reader.result });
+				}
+			};
+			reader.readAsDataURL(event.target.files[0]);
+		} catch (error) {}
+	};
 
-    render() {
-        return (
-            <div className="RegistroAdopcion">
-                <div className="NavBarRegistrosAdopcion">
-                    <NavBarRegistros
-                        tabIndicatorPosition={"75%"}
-                        activePosition={"Adopcion"}
-                    />
-                </div>
-                <div className="FormularioAdopcion">
-                    <div className="DatosGeneralesAdopcion">
-                        <DatosGenerales
-                            handleChange={this.handleChange}
-                            visitaDeAdopcion={this.state.visitaDeAdopcion}
-                            adoptante={this.state.adoptante}
-                            adoptado={this.state.adoptado}
-                            telefono={this.state.telefono}
-                            fechaAdopcion={this.state.fechaAdopcion}
-                            medioAdopcion={this.state.medioAdopcion}
-                        />
-                    </div>
-                    <div className="DireccionAdopcion">
-                        <Direccion
-                            handleChange={this.handleChange}
-                            calle={this.state.calle}
-                            numero={this.state.numero}
-                            colonia={this.state.colonia}
-                            municipio={this.state.municipio}
-                        />
-                    </div>
-                    <div className="DataGridAdopcion">
-                        <DataGrid
-                            data={this.state.comentarios}
-                            modifyRow={this.modifyRow}
-                            addRow={this.addRow}
-                            deleteRow={this.deleteRow}
-                        />
-                    </div>
-                </div>
+	render() {
+		return (
+			<div className="RegistroAdopcion">
+				<div className="NavBarRegistrosAdopcion">
+					<NavBarRegistros
+						tabIndicatorPosition={"75%"}
+						activePosition={"Adopcion"}
+					/>
+				</div>
+				<div className="FormularioAdopcion">
+					<div className="DatosGeneralesAdopcion">
+						<DatosGenerales
+							handleChange={this.handleChange}
+							visitaDeAdopcion={this.state.visitaDeAdopcion}
+							adoptante={this.state.adoptante}
+							adoptado={this.state.adoptado}
+							telefono={this.state.telefono}
+							fechaAdopcion={this.state.fechaAdopcion}
+							medioAdopcion={this.state.medioAdopcion}
+						/>
+					</div>
+					<div className="DireccionAdopcion">
+						<Direccion
+							handleChange={this.handleChange}
+							calle={this.state.calle}
+							numero={this.state.numero}
+							colonia={this.state.colonia}
+							municipio={this.state.municipio}
+						/>
+					</div>
+					<div className="DataGridAdopcion">
+						<DataGrid
+							data={this.state.comentarios}
+							modifyRow={this.modifyRow}
+							addRow={this.addRow}
+							deleteRow={this.deleteRow}
+						/>
+					</div>
+				</div>
 
-                <div className="BotonesRegistroAdopcion">
-                    <Link to="/HogarTemporal">
-                        <button className="BotonAdopcionTransicion BotonAnteriorAdopcion">
-                            <i className="fa fa-chevron-circle-left fa-fw"></i>
-                            Hogar Temporal
-                        </button>
-                    </Link>
+				<div className="BotonesRegistroAdopcion">
+					<Link to="/HogarTemporal">
+						<button className="BotonAdopcionTransicion BotonAnteriorAdopcion">
+							<i className="fa fa-chevron-circle-left fa-fw"></i>
+							Hogar Temporal
+						</button>
+					</Link>
 
-                    <button
-                        className="BotonAdopcionRestablecer BotonCentralAdopcion"
-                        onClick={this.handleRestablecer}
-                    >
-                        Restablecer
-                        <i className="fa fa-eraser fa-fw"></i>
-                    </button>
-                    <button
-                        className="BotonAdopcionGuardar BotonCentralAdopcion"
-                        onClick={this.handleSubmit}
-                    >
-                        Registrar
-                        <i className="fa fa-save fa-fw"></i>
-                    </button>
+					<button
+						className="BotonAdopcionRestablecer BotonCentralAdopcion"
+						onClick={this.handleRestablecer}
+					>
+						Restablecer
+						<i className="fa fa-eraser fa-fw"></i>
+					</button>
+					<button
+						className="BotonAdopcionGuardar BotonCentralAdopcion"
+						onClick={this.handleSubmit}
+					>
+						Registrar
+						<i className="fa fa-save fa-fw"></i>
+					</button>
 
-                    <Link to="/RegistroGeneral">
-                        <button className="BotonAdopcionTransicion BotonSiguienteAdopcion">
-                            Registro General
-                            <i className="fa fa-chevron-circle-right fa-fw"></i>
-                        </button>
-                    </Link>
-                </div>
-                <div className="BarraLateralAdopcion flex items-center justify-center b2">
-                    <Foto
-                        id="foto"
-                        foto={this.state.foto}
-                        imageHandler={this.imageHandler}
-                    />
-                </div>
-            </div>
-        );
-    }
+					<Link to="/RegistroGeneral">
+						<button className="BotonAdopcionTransicion BotonSiguienteAdopcion">
+							Registro General
+							<i className="fa fa-chevron-circle-right fa-fw"></i>
+						</button>
+					</Link>
+				</div>
+				<div className="BarraLateralAdopcion flex items-center justify-center b2">
+					<Foto
+						id="foto"
+						foto={this.state.foto}
+						imageHandler={this.imageHandler}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
--- FrontEnd/laika_web/src/Components/ExpedienteMedico/ExpedienteMedico.js
+++ FrontEnd/laika_web/src/Components/ExpedienteMedico/ExpedienteMedico.js
@@ -10,263 +10,265 @@ import Foto from "../SharedComponents/Foto";
 import "./Styles/ExpedienteMedico.css";
 
 class ExpedienteMedico extends Component {
-    state = {
-        /*Diagnóstico*/
-        atropellamiento: false,
-        tvt: false,
-        sarnaPiel: false,
-        viral: false,
-        embarazo: false,
-        cachorros: false,
-        hemoparasitos: false,
-        otro: false,
-        otroEspecificar: "",
+	state = {
+		/*Diagnóstico*/
+		atropellamiento: false,
+		tvt: false,
+		sarnaPiel: false,
+		viral: false,
+		embarazo: false,
+		cachorros: false,
+		hemoparasitos: false,
+		otro: false,
+		otroEspecificar: "",
 
-        /*Esterilización*/
-        esterilizado: "",
-        citaEsterilizacion: "",
-        fechaEsterilizacion: "",
+		/*Esterilización*/
+		esterilizado: "",
+		citaEsterilizacion: "",
+		fechaEsterilizacion: "",
 
-        /*Cartilla de Vacunación*/
-        puppy: false,
-        refuerzoPuppy: false,
-        multiple: false,
-        refuerzoMultiple: false,
-        rabia: false,
-        fechaPuppy: "",
-        fechaRefuerzoPuppy: "",
-        fechaMultiple: "",
-        fechaRefuerzoMultiple: "",
-        fechaRabia: "",
+		/*Cartilla de Vacunación*/
+		puppy: false,
+		refuerzoPuppy: false,
+		multiple: false,
+		refuerzoMultiple: false,
+		rabia: false,
+		fechaPuppy: "",
+		fechaRefuerzoPuppy: "",
+		fechaMultiple: "",
+		fechaRefuerzoMultiple: "",
+		fechaRabia: "",
 
-        /*Fotos*/
-        foto1: "/iconoPerro.png",
-        foto2: "/iconoPerro.png",
-        foto3: "/iconoPerro.png",
+		/*Fotos*/
+		foto1: "/iconoPerro.png",
+		foto2: "/iconoPerro.png",
+		foto3: "/iconoPerro.png",
 
-        /*Tratamiento*/
-        tratamiento: [],
-    };
+		/*Tratamiento*/
+		tratamiento: [],
+	};
 
-    /*Manejador de imágenes*/
-    imageHandler = (event) => {
-        const reader = new FileReader();
-        const foto = event.target.id;
+	/*Manejador de imágenes*/
+	imageHandler = (event) => {
+		try {
+			const reader = new FileReader();
+			const foto = event.target.id;
 
-        reader.onload = () => {
-            if (reader.readyState === 2) {
-                this.setState({ [foto]: reader.result });
-            }
-        };
-        console.log(event.target.id);
-        reader.readAsDataURL(event.target.files[0]);
-    };
+			reader.onload = () => {
+				if (reader.readyState === 2) {
+					this.setState({ [foto]: reader.result });
+				}
+			};
+			console.log(event.target.id);
+			reader.readAsDataURL(event.target.files[0]);
+		} catch (error) {}
+	};
 
-    /*Manejador de eventos*/
-    handleChange = (event) => {
-        const value =
-            event.target.type === "checkbox"
-                ? event.target.checked
-                : event.target.value;
-        this.setState({
-            ...this.state,
-            [event.target.name]: value,
-        });
-        console.log(event.target.name);
-        console.log(value);
-    };
+	/*Manejador de eventos*/
+	handleChange = (event) => {
+		const value =
+			event.target.type === "checkbox"
+				? event.target.checked
+				: event.target.value;
+		this.setState({
+			...this.state,
+			[event.target.name]: value,
+		});
+		console.log(event.target.name);
+		console.log(value);
+	};
 
-    /*Manejador del botón submit*/
-    handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-    };
+	/*Manejador del botón submit*/
+	handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(this.state);
+	};
 
-    /*Manejadores de Restablecer*/
-    handleRestablecer = () => {
-        this.setState({
-            atropellamiento: false,
-            tvt: false,
-            sarnaPiel: false,
-            viral: false,
-            embarazo: false,
-            cachorros: false,
-            hemoparasitos: false,
-            otro: false,
-            otroEspecificar: "",
-            esterilizado: "",
-            citaEsterilizacion: "",
-            fechaEsterilizacion: "",
-            puppy: false,
-            refuerzoPuppy: false,
-            multiple: false,
-            refuerzoMultiple: false,
-            rabia: false,
-            fechaPuppy: "",
-            fechaRefuerzoPuppy: "",
-            fechaMultiple: "",
-            fechaRefuerzoMultiple: "",
-            fechaRabia: "",
-            foto1: "/iconoPerro.png",
-            foto2: "/iconoPerro.png",
-            foto3: "/iconoPerro.png",
-        });
-    };
+	/*Manejadores de Restablecer*/
+	handleRestablecer = () => {
+		this.setState({
+			atropellamiento: false,
+			tvt: false,
+			sarnaPiel: false,
+			viral: false,
+			embarazo: false,
+			cachorros: false,
+			hemoparasitos: false,
+			otro: false,
+			otroEspecificar: "",
+			esterilizado: "",
+			citaEsterilizacion: "",
+			fechaEsterilizacion: "",
+			puppy: false,
+			refuerzoPuppy: false,
+			multiple: false,
+			refuerzoMultiple: false,
+			rabia: false,
+			fechaPuppy: "",
+			fechaRefuerzoPuppy: "",
+			fechaMultiple: "",
+			fechaRefuerzoMultiple: "",
+			fechaRabia: "",
+			foto1: "/iconoPerro.png",
+			foto2: "/iconoPerro.png",
+			foto3: "/iconoPerro.png",
+		});
+	};
 
-    /*Manejador de datagrid medico*/
-    addRow = (event) => {
-        event.preventDefault();
-        const newRow = {
-            id: shortid.generate(),
-            fechaInicio: "",
-            fechaFinal: "",
-            comentarios: "",
-            accion: "",
-            citaMedica: "",
-        };
-        this.setState((state) => ({
-            tratamiento: [newRow, ...state.tratamiento],
-        }));
-    };
+	/*Manejador de datagrid medico*/
+	addRow = (event) => {
+		event.preventDefault();
+		const newRow = {
+			id: shortid.generate(),
+			fechaInicio: "",
+			fechaFinal: "",
+			comentarios: "",
+			accion: "",
+			citaMedica: "",
+		};
+		this.setState((state) => ({
+			tratamiento: [newRow, ...state.tratamiento],
+		}));
+	};
 
-    deleteRow = (id) => {
-        this.setState((state) => ({
-            tratamiento: state.tratamiento.filter((row) => row.id !== id),
-        }));
-    };
+	deleteRow = (id) => {
+		this.setState((state) => ({
+			tratamiento: state.tratamiento.filter((row) => row.id !== id),
+		}));
+	};
 
-    modifyRow = (event) => {
-        let dataTemp = this.state.tratamiento;
+	modifyRow = (event) => {
+		let dataTemp = this.state.tratamiento;
 
-        dataTemp.forEach((element) => {
-            if (element.id === event.target.id) {
-                if (event.target.name === "fechaInicio")
-                    element.fechaInicio = event.target.value;
-                else if (event.target.name === "fechaFinal")
-                    element.fechaFinal = event.target.value;
-                else if (event.target.name === "comentarios")
-                    element.comentarios = event.target.value;
-                else if (event.target.name === "accion")
-                    element.accion = event.target.value;
-                else if (event.target.name === "citaMedica")
-                    element.citaMedica = event.target.value;
-            }
-        });
+		dataTemp.forEach((element) => {
+			if (element.id === event.target.id) {
+				if (event.target.name === "fechaInicio")
+					element.fechaInicio = event.target.value;
+				else if (event.target.name === "fechaFinal")
+					element.fechaFinal = event.target.value;
+				else if (event.target.name === "comentarios")
+					element.comentarios = event.target.value;
+				else if (event.target.name === "accion")
+					element.accion = event.target.value;
+				else if (event.target.name === "citaMedica")
+					element.citaMedica = event.target.value;
+			}
+		});
 
-        this.setState({
-            tratamiento: dataTemp,
-        });
-    };
+		this.setState({
+			tratamiento: dataTemp,
+		});
+	};
 
-    /*Expediente Médico*/
-    render() {
-        return (
-            <div className="RegistroMedico">
-                <div className="NavBarRegistrosMedico">
-                    <NavBarRegistros
-                        tabIndicatorPosition={"25%"}
-                        activePosition={"ExpedienteMedico"}
-                    />
-                </div>
-                <div className="FormularioMedico">
-                    <div className="diagnostico">
-                        <Diagnostico
-                            atropellamiento={this.state.atropellamiento}
-                            tvt={this.state.tvt}
-                            sarnaPiel={this.state.sarnaPiel}
-                            viral={this.state.viral}
-                            embarazo={this.state.embarazo}
-                            cachorros={this.state.cachorros}
-                            hemoparasitos={this.state.hemoparasitos}
-                            otro={this.state.otro}
-                            otroEspecificar={this.state.otroEspecificar}
-                            handleChange={this.handleChange}
-                        />
-                    </div>
-                    <div className="esterilizacion">
-                        <Esterilizacion
-                            esterilizado={this.state.esterilizado}
-                            citaEsterilizacion={this.state.citaEsterilizacion}
-                            fechaEsterilizacion={this.state.fechaEsterilizacion}
-                            handleChange={this.handleChange}
-                        />
-                    </div>
-                    <div className="cartillaVacunacion">
-                        <CartillaVacunacion
-                            puppy={this.state.puppy}
-                            refuerzoPuppy={this.state.refuerzoPuppy}
-                            multiple={this.state.multiple}
-                            refuerzoMultiple={this.state.refuerzoMultiple}
-                            rabia={this.state.rabia}
-                            fechaPuppy={this.state.fechaPuppy}
-                            fechaRefuerzoPuppy={this.state.fechaRefuerzoPuppy}
-                            fechaMultiple={this.state.fechaMultiple}
-                            fechaRefuerzoMultiple={
-                                this.state.fechaRefuerzoMultiple
-                            }
-                            fechaRabia={this.state.fechaRabia}
-                            handleChange={this.handleChange}
-                        />
-                    </div>
-                    <div className="tratamiento">
-                        <DataGridMed
-                            data={this.state.tratamiento}
-                            modifyRow={this.modifyRow}
-                            addRow={this.addRow}
-                            deleteRow={this.deleteRow}
-                        />
-                    </div>
-                </div>
-                <div className="BotonesRegistroMedico">
-                    <Link to="/RegistroGeneral">
-                        <button className="BotonMedicoTransicion BotonAnteriorMedico">
-                            {" "}
-                            <i className="fa fa-chevron-circle-left fa-fw"></i>
-                            Registro General
-                        </button>
-                    </Link>
-                    <button
-                        className="BotonMedicoRestablecer BotonCentralMedico"
-                        onClick={this.handleRestablecer}
-                    >
-                        Restablecer <i className="fa fa-eraser fa-fw"></i>
-                    </button>
-                    <button
-                        className="BotonMedicoGuardar BotonCentralMedico"
-                        onClick={this.handleSubmit}
-                    >
-                        Guardar <i className="fa fa-save fa-fw"></i>
-                    </button>
-                    <Link to="/HogarTemporal">
-                        <button className="BotonMedicoTransicion BotonSiguienteMedico">
-                            Hogar Temporal{" "}
-                            <i className="fa fa-chevron-circle-right fa-fw"></i>
-                        </button>
-                    </Link>
-                </div>
-                <div className="BarraLateralMedico flex items-center justify-center flex-column justify-between pv3">
-                    <Foto
-                        id="foto1"
-                        className="pt3"
-                        foto={this.state.foto1}
-                        imageHandler={this.imageHandler}
-                    />
-                    <Foto
-                        id="foto2"
-                        className="pt3"
-                        foto={this.state.foto2}
-                        imageHandler={this.imageHandler}
-                    />
-                    <Foto
-                        id="foto3"
-                        className="pt3"
-                        foto={this.state.foto3}
-                        imageHandler={this.imageHandler}
-                    />
-                </div>
-            </div>
-        );
-    }
+	/*Expediente Médico*/
+	render() {
+		return (
+			<div className="RegistroMedico">
+				<div className="NavBarRegistrosMedico">
+					<NavBarRegistros
+						tabIndicatorPosition={"25%"}
+						activePosition={"ExpedienteMedico"}
+					/>
+				</div>
+				<div className="FormularioMedico">
+					<div className="diagnostico">
+						<Diagnostico
+							atropellamiento={this.state.atropellamiento}
+							tvt={this.state.tvt}
+							sarnaPiel={this.state.sarnaPiel}
+							viral={this.state.viral}
+							embarazo={this.state.embarazo}
+							cachorros={this.state.cachorros}
+							hemoparasitos={this.state.hemoparasitos}
+							otro={this.state.otro}
+							otroEspecificar={this.state.otroEspecificar}
+							handleChange={this.handleChange}
+						/>
+					</div>
+					<div className="esterilizacion">
+						<Esterilizacion
+							esterilizado={this.state.esterilizado}
+							citaEsterilizacion={this.state.citaEsterilizacion}
+							fechaEsterilizacion={this.state.fechaEsterilizacion}
+							handleChange={this.handleChange}
+						/>
+					</div>
+					<div className="cartillaVacunacion">
+						<CartillaVacunacion
+							puppy={this.state.puppy}
+							refuerzoPuppy={this.state.refuerzoPuppy}
+							multiple={this.state.multiple}
+							refuerzoMultiple={this.state.refuerzoMultiple}
+							rabia={this.state.rabia}
+							fechaPuppy={this.state.fechaPuppy}
+							fechaRefuerzoPuppy={this.state.fechaRefuerzoPuppy}
+							fechaMultiple={this.state.fechaMultiple}
+							fechaRefuerzoMultiple={
+								this.state.fechaRefuerzoMultiple
+							}
+							fechaRabia={this.state.fechaRabia}
+							handleChange={this.handleChange}
+						/>
+					</div>
+					<div className="tratamiento">
+						<DataGridMed
+							data={this.state.tratamiento}
+							modifyRow={this.modifyRow}
+							addRow={this.addRow}
+							deleteRow={this.deleteRow}
+						/>
+					</div>
+				</div>
+				<div className="BotonesRegistroMedico">
+					<Link to="/RegistroGeneral">
+						<button className="BotonMedicoTransicion BotonAnteriorMedico">
+							{" "}
+							<i className="fa fa-chevron-circle-left fa-fw"></i>
+							Registro General
+						</button>
+					</Link>
+					<button
+						className="BotonMedicoRestablecer BotonCentralMedico"
+						onClick={this.handleRestablecer}
+					>
+						Restablecer <i className="fa fa-eraser fa-fw"></i>
+					</button>
+					<button
+						className="BotonMedicoGuardar BotonCentralMedico"
+						onClick={this.handleSubmit}
+					>
+						Guardar <i className="fa fa-save fa-fw"></i>
+					</button>
+					<Link to="/HogarTemporal">
+						<button className="BotonMedicoTransicion BotonSiguienteMedico">
+							Hogar Temporal{" "}
+							<i className="fa fa-chevron-circle-right fa-fw"></i>
+						</button>
+					</Link>
+				</div>
+				<div className="BarraLateralMedico flex items-center justify-center flex-column justify-between pv3">
+					<Foto
+						id="foto1"
+						className="pt3"
+						foto={this.state.foto1}
+						imageHandler={this.imageHandler}
+					/>
+					<Foto
+						id="foto2"
+						className="pt3"
+						foto={this.state.foto2}
+						imageHandler={this.imageHandler}
+					/>
+					<Foto
+						id="foto3"
+						className="pt3"
+						foto={this.state.foto3}
+						imageHandler={this.imageHandler}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default ExpedienteMedico;
