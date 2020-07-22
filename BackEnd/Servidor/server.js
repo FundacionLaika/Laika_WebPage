9076758af2ea93c58dfda24bb78e94f9f2cca432
ReyVega <a01114523@itesm.mx>
2020-07-22 00:11:44 -0500

    Dependencias esenciales agregadas en server.js, también hay codigo para poner a prueba el nodemon

--- BackEnd/Servidor/server.js
+++ BackEnd/Servidor/server.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const cors = require('cors');
+const knex = require('knex');
+
 const app = express();
+
+const db = knex({
+    client: '',
+    connection: {
+        host: '',
+        user: '',
+        password: '',
+        database: ''
+    }
+});
 
 app.get('/', (req,res) => {
     res.send('this is working');
