46c97be7d45807aac4cb31993af502d0768f084d
ReyVega <a01114523@itesm.mx>
2020-08-13 09:35:23 -0500

    DatosGeneralesPDF terminado

--- FrontEnd/laika_web/src/Components/SharedComponents/BotonPDF.js
+++ FrontEnd/laika_web/src/Components/SharedComponents/BotonPDF.js
@@ -27,10 +27,10 @@ function Reducer(state, action) {
 	}
 }
 
-function BotonPDF() {
+function BotonPDF(props) {
 	url =
 		"/PDF/?id=" +
-		this.props.id +
+		props.id +
 		"&datosGenerales=" +
 		datosGenerales +
 		"&expedienteMedico=" +
