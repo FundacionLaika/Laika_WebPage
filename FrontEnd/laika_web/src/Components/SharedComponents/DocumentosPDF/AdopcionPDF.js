7b60d5664d789f50b94b793017840b0018880d8d
ReyVega <a01114523@itesm.mx>
2020-08-13 10:12:59 -0500

    AdopcionPDF terminado

--- FrontEnd/laika_web/src/Components/SharedComponents/DocumentosPDF/AdopcionPDF.js
+++ FrontEnd/laika_web/src/Components/SharedComponents/DocumentosPDF/AdopcionPDF.js
@@ -9,23 +9,55 @@ import {
 } from "@david.kucsai/react-pdf-table";
 
 class AdopcionPDF extends React.Component {
+	formatDate = (date) => {
+		var d = new Date(date),
+			month = "" + (d.getMonth() + 1),
+			day = "" + d.getDate(),
+			year = d.getFullYear();
+
+		if (month.length < 2) month = "0" + month;
+		if (day.length < 2) day = "0" + day;
+
+		return [day, month, year].join("/");
+	};
+
 	render() {
 		return (
 			<>
-				<Text style={styles.subtitle} break>
-					Adopción
+				<Text style={styles.subtitle}>Adopción</Text>
+				<Text style={styles.text}>
+					{"Adoptante: " + this.props.data.adoptante}
 				</Text>
-				<Text style={styles.text}>Adoptante:</Text>
-				<Text style={styles.text}>Adoptado:</Text>
-				<Text style={styles.text}>Medio:</Text>
-				<Text style={styles.text}>Teléfono:</Text>
-				<Text style={styles.text}>Visita de adopción:</Text>
-				<Text style={styles.text}>Fecha de adopción:</Text>
+				<Text style={styles.text}>
+					{"Adoptado: " + this.props.data.adoptado}
+				</Text>
+				<Text style={styles.text}>
+					{"Medio: " + this.props.data.medioAdopcion}
+				</Text>
+				<Text style={styles.text}>
+					{"Teléfono: " + this.props.data.telefono}
+				</Text>
+				<Text style={styles.text}>
+					{"Visita de adopción: " +
+						this.formatDate(this.props.data.visitaDeAdopcion)}
+				</Text>
+				<Text style={styles.text}>
+					{"Fecha de adopción: " +
+						this.formatDate(this.props.data.fechaAdopcion)}
+				</Text>
 				<Text style={styles.subtitle}>Dirección</Text>
-				<Text style={styles.text}>Calle:</Text>
-				<Text style={styles.text}>Número:</Text>
-				<Text style={styles.text}>Colonia:</Text>
-				<Text style={styles.text}>Municipio:</Text>
+				<Text style={styles.text}>
+					{"Calle: " + this.props.data.calle}
+				</Text>
+				<Text style={styles.text}>
+					{"Número: " + this.props.data.numero}
+				</Text>
+				<Text style={styles.text}>
+					{"Colonia: " + this.props.data.colonia}
+				</Text>
+				<Text style={styles.text}>
+					{"Municipio: " + this.props.data.municipio}
+				</Text>
 				<Image
 					style={styles.image}
 					src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Icecat1-300x300.svg/1200px-Icecat1-300x300.svg.png"
