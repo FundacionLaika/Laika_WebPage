68e3039a26b747922531db5291febb65635cf912
AgusQuintanar <agusquintanar17@gmail.com>
2021-04-10 15:05:47 -0500

    fixes bad request bug in grid and adds animal counter to grids

--- frontend/src/Components/Consulta/Subcomponentes/Grid/SubGrids/GridExpedienteMedico.js
+++ frontend/src/Components/Consulta/Subcomponentes/Grid/SubGrids/GridExpedienteMedico.js
@@ -1,65 +1,77 @@
 import React from "react";
 import TarjetaExpedienteMedico from "../Tarjetas/TarjetaExpedienteMedico";
+import "./Styles/Subgrid.css";
 
 export default class GridExpedienteMedico extends React.Component {
-	state = {
-		data: []	
-	}
-	fetchData = () => {
-		fetch("http://localhost:3001/consulta", {
-			method: "post",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(this.props.filtros),
-		})
-			.then((response) => response.json())
-			.then((response) => {
-				this.setState({
-					data: response,
-				});
+    state = {
+        data: [],
+    };
+    fetchData = () => {
+        fetch("http://localhost:3001/consulta", {
+            method: "post",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(this.props.filtros),
+        })
+            .then((response) => {
+				if (response.ok) return response.json();
+				else return [];
 			})
-			.catch((err) => console.log(err));
-	}
+            .then((response) => {
+                this.setState({
+                    data: response,
+                });
+            })
+            .catch((err) => console.log(err));
+    };
 
-	componentDidMount() {
-		this.fetchData();
-	}
+    componentDidMount() {
+        this.fetchData();
+    }
 
-	componentDidUpdate(prevProps, prevState) {
-		if (this.props.filtros !== prevProps.filtros) {
-			this.fetchData();
-		}
-	}
+    componentDidUpdate(prevProps, prevState) {
+        if (this.props.filtros !== prevProps.filtros) {
+            this.fetchData();
+        }
+    }
 
-	render() {
-		return (
-			<div>
-				{this.state.data.map((tarjeta) => (
-					<TarjetaExpedienteMedico
-						key={tarjeta.ID_Animal}
-						id={tarjeta.ID_Animal}
-						foto={tarjeta.Foto}
-						atropellamiento={tarjeta.Atropellamiento}
-						tvt={tarjeta.TVT}
-						sarnaPiel={tarjeta.Sarna_Piel}
-						viral={tarjeta.Viral}
-						embarazo={tarjeta.Embarazo}
-						cachorros={tarjeta.Cachorros}
-						hemoparasitos={tarjeta.Hemoparasitos}
-						otro={tarjeta.Otro}
-						puppy={tarjeta.Puppy}
-						refuerzoPuppy={tarjeta.RefuerzoPuppy}
-						multiple={tarjeta.Multiple}
-						refuerzoMultiple={tarjeta.RefuerzoMultiple}
-						rabia={tarjeta.Rabia}
-						fechaEsterilizacion={tarjeta.FechaEsterilizacion}
-						citaAgendada={tarjeta.CitaAgendada}
-						estaEsterilizado={tarjeta.EstaEsterilizado}
-						formatDate={this.props.formatDate}
-						openModal={this.props.openModal}
-						setID={this.props.setID}
-					/>
-				))}
-			</div>
-		);
-	}
+    render() {
+        if (!this.state.data) return null;
+        return (
+            <div className="subGrid">
+                <div className="animalCounter">
+                    <span>
+                        {"Animales encontrados: " + this.state.data.length}
+                    </span>
+                </div>
+                <div>
+                    {this.state.data.map((tarjeta) => (
+                        <TarjetaExpedienteMedico
+                            key={tarjeta.ID_Animal}
+                            id={tarjeta.ID_Animal}
+                            foto={tarjeta.Foto}
+                            atropellamiento={tarjeta.Atropellamiento}
+                            tvt={tarjeta.TVT}
+                            sarnaPiel={tarjeta.Sarna_Piel}
+                            viral={tarjeta.Viral}
+                            embarazo={tarjeta.Embarazo}
+                            cachorros={tarjeta.Cachorros}
+                            hemoparasitos={tarjeta.Hemoparasitos}
+                            otro={tarjeta.Otro}
+                            puppy={tarjeta.Puppy}
+                            refuerzoPuppy={tarjeta.RefuerzoPuppy}
+                            multiple={tarjeta.Multiple}
+                            refuerzoMultiple={tarjeta.RefuerzoMultiple}
+                            rabia={tarjeta.Rabia}
+                            fechaEsterilizacion={tarjeta.FechaEsterilizacion}
+                            citaAgendada={tarjeta.CitaAgendada}
+                            estaEsterilizado={tarjeta.EstaEsterilizado}
+                            formatDate={this.props.formatDate}
+                            openModal={this.props.openModal}
+                            setID={this.props.setID}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
